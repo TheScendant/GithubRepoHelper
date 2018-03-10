a2d315ce83a36f811e2a22179181bd53c30c42d7
hmn8929 <hayden518@gmail.com>
2018-03-10 01:13:15 -0500

    handling response. app.config for globals

--- app/server/server.py
+++ app/server/server.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template,redirect
+from flask import Flask, render_template,redirect,request
+import requests
+from urlparse import parse_qs
+from urllib import urlencode
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
-
+app.config['CLIENT_ID'] = 'b8b55a398f9ccbfb603d'
+app.config['CLIENT_SECRET'] = 'fa57ad5bbeb1202997f6c5cd5574b2d7f1c4be1a'
+app.config['CODE_URL'] = 'https://github.com/login/oauth/authorize/?'
+app.config['TOKEN_URL'] = 'https://github.com/login/oauth/access_token/?'
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -12,11 +18,19 @@ def get_hello():
 
 @app.route("/auth/login")
 def send_to_git():
-    return redirect("https://github.com/login/oauth/authorize?client_id=b8b55a398f9ccbfb603d?redirect_uri=/login/oauth/callback")
+    return redirect(app.config['CODE_URL'] +urlencode({"client_id" : app.config['CLIENT_ID'] }))
 
 @app.route("/login/oauth/callback")
 def return_from_git():
-    return "Holy shit i got something back"
+    code = request.args.get('code')
+    postData = {
+        'client_id':app.config['CLIENT_ID'],
+        'client_secret': app.config['CLIENT_SECRET'],
+        'code': code
+    }
+    x = requests.post(app.config['TOKEN_URL'],data = postData)
+    result = parse_qs(x.text)
+    return "we done good"
 
 
 if __name__ == "__main__":
